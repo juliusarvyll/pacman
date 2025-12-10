373b286dd96f0a2a9e37e1b5af16a53a52f21900
juliusarvyll <juliuscepeda28@gmail.com>
2025-12-10 14:02:42 +0800

    kjljkl

--- 32px-pokemoni.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="32px-pokemoni" tilewidth="32" tileheight="32" tilecount="775" columns="25">
- <image source="public/game/32px-pokemoni.png" width="816" height="1020"/>
-</tileset>
